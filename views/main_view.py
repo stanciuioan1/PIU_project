295a8b71678ac24fa4d28057f68285a43a73794d
Alin Stoian <alin-bogdan.stoian@student.tuiasi.ro>
2021-11-10 17:36:26 +0200

    Laborator

--- views/main_view.py
+++ views/main_view.py
@@ -1,4 +1,5 @@
-from PySide6.QtWidgets import QMainWindow
+from PySide6.QtWidgets import QMainWindow, QMenuBar, QVBoxLayout, QPushButton, QWidget \
+    , QMenuBar, QMenu
 
 
 '''
@@ -15,6 +16,27 @@ class MainView(QMainWindow):
         super().__init__()
         self._model = model
         self.main_controller = main_controller
-        
+        self.setWindowTitle("SubMaker 2021")
+        #self.resize(400,200)
+        main_vbox = QVBoxLayout()
+        main_vbox.addStretch(1)
+        '''
+        button1 = QPushButton(text="1")
+        main_vbox.addWidget(button1)
+        button2 = QPushButton(text="2")
+        main_vbox.addWidget(button2)
+        '''
+        menuBar = QMenuBar()   
+
+        fileMenu = QMenu("&File", self)
+        menuBar.addMenu(fileMenu)
+        self.setMenuBar(menuBar)
+
+    
+
+        widget = QWidget()
+        widget.setLayout(main_vbox)
+        self.setCentralWidget(widget)
+
         # ... connect-uri cu gramada intre ce definim aici si ce e in controller
         # de asemenea, ascultam pentru semnale din model
